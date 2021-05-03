bc618ec280848a7ab5f0da0f326b886f1b6076f8
Matheus Serrao <maktheus@gmail.com>
2021-05-03 12:19:10 -0400

    init

---
+++ main.py
@@ -0,0 +1,76 @@
+from pypylon import pylon
+from pypylon import genicam
+import cv2
+import pandas as pd
+import sys
+
+# Number of images to be grabbed.
+countOfImagesToGrab = 100
+
+# The exit code of the sample application.
+exitCode = 0
+
+try:
+    # Create an instant camera object with the camera device found first.
+    camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+
+
+    camera.Open()
+
+
+
+    # Print the model name of the camera.
+    print("Using device ", camera.GetDeviceInfo().GetModelName())
+
+    # demonstrate some feature access
+    new_width = camera.Width.GetValue() - camera.Width.GetInc()
+    if new_width >= camera.Width.GetMin():
+        camera.Width.SetValue(new_width)
+
+    # The parameter MaxNumBuffer can be used to control the count of buffers
+    # allocated for grabbing. The default value of this parameter is 10.
+    camera.MaxNumBuffer = 5
+
+    # Start the grabbing of c_countOfImagesToGrab images.
+    # The camera device is parameterized with a default configuration which
+    # sets up free-running continuous acquisition.
+    # Grabing Continusely (video) with minimal delay
+    camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
+    converter = pylon.ImageFormatConverter()
+
+    # converting to opencv bgr format
+    converter.OutputPixelFormat = pylon.PixelType_BGR8packed
+    converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
+
+    # Camera.StopGrabbing() is called automatically by the RetrieveResult() method
+    # when c_countOfImagesToGrab images have been retrieved.
+    while camera.IsGrabbing():
+        # Wait for an image and then retrieve it. A timeout of 5000 ms is used.
+        grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+
+        # Image grabbed successfully?
+        if grabResult.GrabSucceeded():
+            # Access the image data.
+
+            image = converter.Convert(grabResult)
+            img = image.GetArray()
+            print(img)
+
+            cv2.namedWindow('title', cv2.WINDOW_NORMAL)
+            cv2.imshow('title', img)
+            k = cv2.waitKey(1)
+            if k == 27:
+                break
+        else:
+            print("Error: ", grabResult.ErrorCode, grabResult.ErrorDescription)
+        grabResult.Release()
+    camera.Close()
+    cv2.destroyAllWindows()
+
+except genicam.GenericException as e:
+    # Error handling.
+    print("An exception occurred.")
+    print(e.GetDescription())
+    exitCode = 1
+
+sys.exit(exitCode)
